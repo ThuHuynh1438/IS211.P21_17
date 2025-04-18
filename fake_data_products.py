1ac60296bb6bec1cf8b47fe8d0e460a39d2957b9
nhxttrxng <162859262+nhxttrxng@users.noreply.github.com>
2025-04-18 20:59:18 +0700

    sua thanh không đâu

--- fake_data_products.py
+++ fake_data_products.py
@@ -1,40 +1,41 @@
 from faker import Faker
 import random
 from datetime import timedelta
+
 fake = Faker()
 
 categories = [
-    "Sữa tươi", "Sữa chua", "Sữa đặc", "Sữa hạt", 
-    "Sữa công thức", "Sữa không đường", "Sữa hữu cơ"
+    "Sua tuoi", "Sua chua", "Sua dac", "Sua hat", 
+    "Sua cong thuc", "Sua khong duong", "Sua huu co"
 ]
 
-origins = ["Việt Nam", "Mỹ", "Úc", "New Zealand", "Hà Lan", "Nhật Bản", "Đức", "Thái Lan"]
+origins = ["Viet Nam", "My", "Uc", "New Zealand", "Ha Lan", "Nhat Ban", "Duc", "Thai Lan"]
 
 brands = [
     "Vinamilk", "TH True Milk", "Nutifood", "Dutch Lady", 
-    "Nestlé", "Morinaga", "Meiji", "Ensure", 
-    "Friso", "Abbott", "Colosbaby", "GrowPLUS+", 
-    "Anlene", "Fami", "Ovaltine", "Milo", "Lactel", "Love’in Farm"
+    "Nestle", "Morinaga", "Meiji", "Ensure", 
+    "Friso", "Abbott", "Colosbaby", "GrowPLUS", 
+    "Anlene", "Fami", "Ovaltine", "Milo", "Lactel", "Lovein Farm"
 ]
 
 types = [
-    "Sữa tươi tiệt trùng", "Sữa chua uống", "Sữa đặc có đường", 
-    "Sữa hạt óc chó", "Sữa hạnh nhân", "Sữa công thức", 
-    "Sữa bột pha sẵn", "Sữa không lactose", "Sữa hữu cơ"
+    "Sua tuoi tiet trung", "Sua chua uong", "Sua dac co duong", 
+    "Sua hat oc cho", "Sua hanh nhan", "Sua cong thuc", 
+    "Sua bot pha san", "Sua khong lactose", "Sua huu co"
 ]
 
 variants = [
-    "1L", "180ml", "200ml", "Có đường", "Ít đường", 
-    "Không đường", "Hương Dâu", "Hương Chuối", 
-    "Hương Socola", "Gold", "Organic", "Dành cho bé", 
-    "Người lớn tuổi", "Dinh dưỡng cao"
+    "1L", "180ml", "200ml", "Co duong", "It duong", 
+    "Khong duong", "Huong Dau", "Huong Chuoi", 
+    "Huong Socola", "Gold", "Organic", "Danh cho be", 
+    "Nguoi lon tuoi", "Dinh duong cao"
 ]
 
 def generate_product_id(i):
     return f"P{i:04d}"
 
 def generate_cost_and_price():
-    cost = round(random.uniform(10_000, 50_000), -2)
+    cost = round(random.uniform(10000, 50000), -2)
     price = round(cost * random.uniform(1.1, 1.5), -2)
     return cost, price
 
@@ -52,7 +53,6 @@ def generate_product(i):
     category = random.choice(categories)
     vat = random.choice([5, 8, 10])
     stock = random.randint(0, 500)
-
     return f"INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME, ORIGIN, COST_PRICE, SELLING_PRICE, PRODUCTION_DATE, EXPIRY_DATE, CATEGORY, VAT, STOCK) VALUES ('{product_id}', '{name}', '{origin}', {cost_price}, {selling_price}, DATE '{prod_date}', DATE '{expiry_date}', '{category}', {vat}, {stock});"
 
 # Tạo ra file txt
@@ -62,4 +62,4 @@ with open(filename, "w", encoding="utf-8") as f:
         insert_stmt = generate_product(i)
         f.write(insert_stmt + "\n")
 
-print("✅ Đã tạo file insert_product_data.txt chứa 1000 dòng dữ liệu.")
+print("✅ Đã tạo file insert_product_data.txt chứa 1000 dòng dữ liệu KHÔNG DẤU.")
